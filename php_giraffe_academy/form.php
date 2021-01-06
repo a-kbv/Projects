85f067ff41b7f122e3f576c25aea61a4f150edc6
a-kbv <atanaskorabov@gmail.com>
2021-01-06 17:25:34 +0200

    updt

--- php_giraffe_academy/form.php
+++ php_giraffe_academy/form.php
@@ -7,41 +7,6 @@
 <body>
 
 <?php
-// working with strings
-$phrase = "Giraffe Academy";
-echo $phrase;
-echo "<br>";
-
-echo strtolower($phrase);
-echo "<br>";
-
-echo strlen($phrase);
-echo "<br>";
-
-echo $phrase[0];
-echo "<br>";
-
-$phrase[0] = "B";
-echo $phrase;
-echo "<br>";
-$phrase[0] = "G";
-
-echo str_replace("Giraffe", "Panda", $phrase);
-echo "<br>";
-
-echo substr($phrase, 0, 7);
-echo "<br>";
-echo substr($phrase, 8);
-echo "<br>";
-
-// Working with numbers
-echo 40;
-echo "<br>";
-echo -59.835;
-echo "<br>";
-echo 10 % 3;
-echo "<br>";
-
 
 
 ?>
